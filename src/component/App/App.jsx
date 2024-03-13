098b6372a90f4c8affc8ebd6bd1315e54914232d
viowl <sovaworkbox@gmail.com>
2024-03-13 22:00:39 +0100

    add import

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -1,5 +1,9 @@
-import TaskList from "../TaskList/TaskList.jsx";
-
+import TaskList from "../TaskList/TaskList";
+import initialTasks from "../../tasks.json";
+import Form from "../Form/Form";
+import Filter from "../Filter/Filter";
+import css from "./App.module.css";
+import { useState } from "react";
 function App() {}
 
 export default App;
